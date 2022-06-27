71961a3ebccce0164b51c80b411d75cac5b6f475
Neal Dooley <nealdooley19@gmail.com>
2022-06-27 09:39:04 -0500

    initial commit

--- addToZero.js
+++ addToZero.js
@@ -1,10 +1,15 @@
 let sumZero = false
-let numbers =[0, 1, 2, 3, 4, 5]
+let numbers = [1, 4, 11, 2, 37, -4]
 for (let i = 0; i < numbers.length; i++) {
     for (let j = i + 1 ; j < numbers.length; j++) {
+        //console.log(numbers[i] + numbers[j])
         if (numbers[i] + numbers[j] === 0){
             sumZero = true
-        }
+            break;
+        } 
+    } 
+    if (sumZero === true) {
+        break;
     }
 }
 console.log(sumZero)
